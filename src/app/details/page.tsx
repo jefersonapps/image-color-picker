d1e30cc27dd970728746f0204bc0a2ca15bbf4d2
jefersonapps <jefersonplaynunes@gmail.com>
2024-07-29 18:38:53 -0300

    fix: wrap components using useSearchParams in Suspense boundary

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -11,6 +11,8 @@ import { useGlobalContext } from "../contexts/global-context";
 import { useImageCanvas } from "../hooks/useImageCanvas";
 import { useMouseEvents } from "../hooks/useMouseEvents";
 import { usePaletteColors } from "../hooks/usePaletteColors";
+import { Suspense } from "react";
+import { Loading } from "../components/loading";
 
 export default function ImageDetailsPage() {
   const { image, pickedColor } = useGlobalContext();
@@ -44,41 +46,43 @@ export default function ImageDetailsPage() {
   }
 
   return (
-    <div className="h-svh overflow-hidden relative pt-14 md:pt-16">
-      <button
-        className="absolute top-2 left-2 md:top-4 md:left-4 z-50 px-4 py-2 rounded-md bg-zinc-800 flex gap-2 hover:bg-zinc-200 hover:text-black shadow-black/50 shadow-md transition-colors"
-        onClick={handleBackClick}
-      >
-        <ArrowLeft /> <span>Voltar</span>
-      </button>
+    <Suspense fallback={<Loading />}>
+      <div className="h-svh overflow-hidden relative pt-14 md:pt-16">
+        <button
+          className="absolute top-2 left-2 md:top-4 md:left-4 z-50 px-4 py-2 rounded-md bg-zinc-800 flex gap-2 hover:bg-zinc-200 hover:text-black shadow-black/50 shadow-md transition-colors"
+          onClick={handleBackClick}
+        >
+          <ArrowLeft /> <span>Voltar</span>
+        </button>
 
-      <div
-        className="h-[calc(100%_-_238px)] md:h-[calc(100%_-_174px)] overflow-auto"
-        ref={containerRef}
-        style={{ paddingBottom: ZOOM_CIRCLE_SIZE + "px" }}
-      >
-        {image && (
-          <ImageCanvas
-            canvasRef={canvasRef}
-            handleCanvasClick={handleCanvasClick}
-            handleMouseEnter={handleMouseEnter}
-            handleMouseLeave={handleMouseLeave}
-            handleMouseMove={handleMouseMove}
-            image={image}
-            isHovering={isHovering}
-            zoomCanvasRef={zoomCanvasRef}
-          />
-        )}
-      </div>
+        <div
+          className="h-[calc(100%_-_238px)] md:h-[calc(100%_-_174px)] overflow-auto"
+          ref={containerRef}
+          style={{ paddingBottom: ZOOM_CIRCLE_SIZE + "px" }}
+        >
+          {image && (
+            <ImageCanvas
+              canvasRef={canvasRef}
+              handleCanvasClick={handleCanvasClick}
+              handleMouseEnter={handleMouseEnter}
+              handleMouseLeave={handleMouseLeave}
+              handleMouseMove={handleMouseMove}
+              image={image}
+              isHovering={isHovering}
+              zoomCanvasRef={zoomCanvasRef}
+            />
+          )}
+        </div>
 
-      <div className="absolute z-50 bottom-0 left-0 right-0 p-2 md:p-4">
-        {data && !loading && !error && (
-          <div className="block justify-center space-y-2 gap-2 md:gap-4 md:flex">
-            {data && !loading && !error && <ColorPalette data={data} />}
-            <ColorInfo hoverColor={hoverColor} pickedColor={pickedColor} />
-          </div>
-        )}
+        <div className="absolute z-50 bottom-0 left-0 right-0 p-2 md:p-4">
+          {data && !loading && !error && (
+            <div className="block justify-center space-y-2 gap-2 md:gap-4 md:flex">
+              {data && !loading && !error && <ColorPalette data={data} />}
+              <ColorInfo hoverColor={hoverColor} pickedColor={pickedColor} />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import { Expand, Github } from "lucide-react";
 import { FileInput } from "./components/file-input";
 import Image from "next/image";
 import { Animation } from "./components/animation";
-import { useCallback } from "react";
+import { Suspense, useCallback } from "react";
 import { useImageCanvas } from "./hooks/useImageCanvas";
 import { useGlobalContext } from "./contexts/global-context";
 import { useImageUpload } from "./hooks/useImageUpload";
@@ -21,6 +21,7 @@ import { useMouseEvents } from "./hooks/useMouseEvents";
 import { usePaletteColors } from "./hooks/usePaletteColors";
 import { DropOverlay } from "./components/drop-overlay";
 import { useScreen } from "./hooks/useScreen";
+import { Loading } from "./components/loading";
 
 export default function Home() {
   const { image, pickedColor } = useGlobalContext();
@@ -64,63 +65,65 @@ export default function Home() {
   }, [params, image]);
 
   return (
-    <main className="space-y-2 md:space-y-4 p-2 md:p-4 overflow-hidden">
-      <DropOverlay />
-      <header className="max-w-5xl mx-auto flex gap-2 items-center pb-2 border-b-[1px] border-zinc-400">
-        <Image
-          src="/icon.png"
-          alt="Ícone do app - a imagem mostra um conta-gotas acima de uma paleta de cores"
-          width="0"
-          height="0"
-          sizes="32px"
-          className="w-8 h-auto"
-        />{" "}
-        <h1 className="font-extrabold text-xl">Image Color Picker</h1>
-        <a
-          className="ml-auto cursor-pointer"
-          href="https://github.com/jefersonapps"
-          rel="noreferrer"
-          target="_blank"
-          title="GitHub"
-        >
-          <Github />
-        </a>
-      </header>
+    <Suspense fallback={<Loading />}>
+      <main className="space-y-2 md:space-y-4 p-2 md:p-4 overflow-hidden">
+        <DropOverlay />
+        <header className="max-w-5xl mx-auto flex gap-2 items-center pb-2 border-b-[1px] border-zinc-400">
+          <Image
+            src="/icon.png"
+            alt="Ícone do app - a imagem mostra um conta-gotas acima de uma paleta de cores"
+            width="0"
+            height="0"
+            sizes="32px"
+            className="w-8 h-auto"
+          />{" "}
+          <h1 className="font-extrabold text-xl">Image Color Picker</h1>
+          <a
+            className="ml-auto cursor-pointer"
+            href="https://github.com/jefersonapps"
+            rel="noreferrer"
+            target="_blank"
+            title="GitHub"
+          >
+            <Github />
+          </a>
+        </header>
 
-      <FileInput onChange={handleImageUpload} />
-
-      {image && (
-        <button
-          onClick={handleDetailsClick}
-          className="bg-emerald-600 hover:bg-emerald-500 text-black font-semibold uppercase flex px-4 py-2 gap-2 rounded-lg mx-auto transition-colors"
-        >
-          <Expand /> Ver Detalhes
-        </button>
-      )}
-
-      <div>
-        {image && (
-          <ImageCanvas
-            canvasRef={canvasRef}
-            handleCanvasClick={handleCanvasClick}
-            handleMouseEnter={handleMouseEnter}
-            handleMouseLeave={handleMouseLeave}
-            handleMouseMove={handleMouseMove}
-            image={image}
-            isHovering={isHovering}
-            zoomCanvasRef={zoomCanvasRef}
-          />
-        )}
-
-        {!image && <Animation />}
-      </div>
-      <div className="block space-y-2 gap-2 md:gap-4 justify-center md:flex">
-        {data && !loading && !error && <ColorPalette data={data} />}
+        <FileInput onChange={handleImageUpload} />
 
         {image && (
-          <ColorInfo hoverColor={hoverColor} pickedColor={pickedColor} />
+          <button
+            onClick={handleDetailsClick}
+            className="bg-emerald-600 hover:bg-emerald-500 text-black font-semibold uppercase flex px-4 py-2 gap-2 rounded-lg mx-auto transition-colors"
+          >
+            <Expand /> Ver Detalhes
+          </button>
         )}
-      </div>
-    </main>
+
+        <div>
+          {image && (
+            <ImageCanvas
+              canvasRef={canvasRef}
+              handleCanvasClick={handleCanvasClick}
+              handleMouseEnter={handleMouseEnter}
+              handleMouseLeave={handleMouseLeave}
+              handleMouseMove={handleMouseMove}
+              image={image}
+              isHovering={isHovering}
+              zoomCanvasRef={zoomCanvasRef}
+            />
+          )}
+
+          {!image && <Animation />}
+        </div>
+        <div className="block space-y-2 gap-2 md:gap-4 justify-center md:flex">
+          {data && !loading && !error && <ColorPalette data={data} />}
+
+          {image && (
+            <ColorInfo hoverColor={hoverColor} pickedColor={pickedColor} />
+          )}
+        </div>
+      </main>
+    </Suspense>
   );
 }
